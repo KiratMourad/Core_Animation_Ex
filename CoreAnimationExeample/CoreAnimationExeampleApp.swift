fce55cb7a8b9f66f85c347ad355df7df99ce46eb
Kirat <mourad.kirat.9@gmail.com>
2024-02-18 18:52:14 +0100

    Initial Commit

---
+++ CoreAnimationExeample/CoreAnimationExeampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreAnimationExeampleApp.swift
+//  CoreAnimationExeample
+//
+//  Created by mac on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoreAnimationExeampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
